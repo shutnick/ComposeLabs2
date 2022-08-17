3341961dcf4352458ebc439d592b354e4cc07c51
ssofienko <ssofienko@life360.com>
2022-08-17 17:41:08 +0300

    Finish labs

--- app/src/main/java/com/sofiienko/game/composelabs2/MainActivity.kt
+++ app/src/main/java/com/sofiienko/game/composelabs2/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.sofiienko.game.composelabs2
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sofiienko.game.composelabs2.ui.theme.ComposeLabs2Theme
@@ -27,10 +40,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun AppView() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     if (shouldShowOnboarding) {
         OnboardingScreen(
-            onContinueClick = {shouldShowOnboarding = false}
+            onContinueClick = { shouldShowOnboarding = false }
         )
     } else {
         Greetings()
@@ -38,42 +51,59 @@ private fun AppView() {
 }
 
 @Composable
-fun Greetings(names: List<String> = listOf("Android", "Compose")) {
+fun Greetings(names: List<String> = List(1000) { "$it" }) {
     Surface(color = MaterialTheme.colorScheme.background) {
-        Column(Modifier.padding(4.dp)) {
-            names.forEach {
-                Greeting(name = it)
+        LazyColumn(Modifier.padding(4.dp)) {
+            items(names) { name ->
+                Greeting(name = name)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    var isExpaned by remember { mutableStateOf(false) }
-    val extraPadding = if (isExpaned) 48.dp else 0.dp
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(4.dp)
+private fun Greeting(name: String) {
+    Card(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1.toFloat())
-                    .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello ")
-                Text(text = "$name!")
-            }
+        CardContent(name = name)
+    }
+}
 
-            OutlinedButton(
-                onClick = { isExpaned = !isExpaned },
-            ) {
-                Text(
-                    text = if (isExpaned) "Show less" else "Show more",
-                    color = MaterialTheme.colorScheme.onPrimary,
+@Composable
+fun CardContent(name: String) {
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .padding(24.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
                 )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1.toFloat())
+        ) {
+            Text(text = "Hello ")
+            Text(
+                text = "$name!",
+                style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.ExtraBold)
+            )
+            if (isExpanded) {
+                Text(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum")
             }
+        }
+
+        IconButton(onClick = { isExpanded = !isExpanded }) {
+            Icon(
+                imageVector = if (isExpanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = if (isExpanded) "Show less" else "Show more"
+            )
         }
     }
 }
@@ -109,6 +139,14 @@ fun DefaultPreview() {
 @Composable
 fun PreviewOnboardingScreen() {
     ComposeLabs2Theme {
-        OnboardingScreen(onContinueClick = {})
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 480, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewOnboardingScreenNight() {
+    ComposeLabs2Theme {
+        Greetings()
     }
 }
--- app/src/main/java/com/sofiienko/game/composelabs2/ui/theme/Theme.kt
+++ app/src/main/java/com/sofiienko/game/composelabs2/ui/theme/Theme.kt
@@ -10,21 +10,27 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val DarkColorPalette = darkColorScheme(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = LightBlue,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    surface = Blue,
+    onSurface = Color.White,
+    onPrimary = Navy
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -41,7 +47,7 @@ private val LightColorScheme = lightColorScheme(
 fun ComposeLabs2Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -49,14 +55,14 @@ fun ComposeLabs2Theme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-        darkTheme -> DarkColorScheme
+        darkTheme -> DarkColorPalette
         else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController((view.context as Activity).window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
